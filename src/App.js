d727693e23f6d65ba387e1f3f86b6729ba196490
Adam Tadesse <adamtadesse9@gmail.com>
2025-01-06 19:24:26 +0300

    Finished the Contact.js of my project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Work from "./components/Work.js";
+import Contact from "./components/Contact.js";
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
       <Work />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+function Contact() {
+  return (
+    <>
+      <div className="contact">
+        <div className="contact__info">
+          <h2>Book a call with me</h2>
+          <p>
+            I would love to have a chat to see how I can help you. The best
+            first step is for us to discuss your project during a free
+            consultation. Then we can move forward from there.
+          </p>
+        </div>
+        <button className="btn">Free consultation</button>
+      </div>
+    </>
+  );
+}
+export default Contact;
