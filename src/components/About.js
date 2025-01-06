734e66484f41828ac15e2c6c99e9c1b2ee107b3b
Adam Tadesse <adamtadesse9@gmail.com>
2025-01-06 17:41:30 +0300

    Finished the About.js of my Project

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,20 @@
+import profile from "../assets/images/image-amy.webp";
 function About() {
-  return <div>Hello react</div>;
+  return (
+    <>
+      <div className="about">
+        <img src={profile} alt="profile" />
+        <div className="about__me">
+          <h2>I'm Amy, and I'd love to work on your next project</h2>
+          <p>
+            I love working with others to create beautiful design solutions. I
+            have designed everything from brand illustrations to complete mobile
+            apps. I am also handy with a camera!
+          </p>
+          <button className="btn">Free consultation</button>
+        </div>
+      </div>
+    </>
+  );
 }
 export default About;
